e171055a89f99856450fc0b53d2cb25e0b7dc3a7
aiderson <d.ryose1123@gmail.com>
2023-01-05 14:38:28 +0900

    テスト表示データ複数行表示完了

--- test_output.php
+++ test_output.php
@@ -9,25 +9,23 @@
     try {
       /// DB接続を試みる
       $db  = new PDO('mysql:host=' . HOSTNAME . ';dbname=' . DATABASE, USERNAME, PASSWORD);
-      $msg = "MySQL への接続確認が取れました。";
-      $alert = "<script type='text/javascript'>alert('" . $msg . "');</script>";
-      echo $alert;
-      // テーブルに登録するINSERT INTO文を変数に格納　VALUESはプレースフォルダーで空の値を入れとく
-      $sql = "select direction, speed, wave, latposition, lngposition from test_windy_data where datetime between subtime(current_timestamp(), '00:05:00') and current_timestamp();";
+      // テーブルに登録するselect文を変数に格納
+      //10時間前までのデータで試験的に行なっている
+      $sql = "select direction, speed, wave, latposition, lngposition from test_windy_data where datetime between subtime(current_timestamp(), '10:00:00') and current_timestamp();";
       // SQLを実行する
       $statement = $db->query($sql);
       // foreach文で配列の中身を一行ずつ出力
-      foreach ($statement as $row) {
-        // データベースのフィールド名で出力
-        $alert2 = "<script type='text/javascript'>alert('" . $row['direction'] . "');</script>";
+      $records = $statement->fetchAll();
+      foreach ($records as $row) {
+        "<script type='text/javascript'>alert('" . $row['direction'] . "');</script>";
         echo $alert2;
-        }
-      } catch (PDOException $e) {
+      }
+    } catch (PDOException $e) {
       $isConnect = false;
       $msg = "MySQL への接続に失敗しました。";
-      }
     }
-/*
+  }
+  /*
   
     //配列の初期化
     /*$windData = array();
@@ -40,7 +38,7 @@
       'lngposition' => $row['lngposition']
     );
 */
-    /*
+  /*
   //PHPの配列をJSON形式のデータに変換
   //$json = json_encode($windData);
   // MySQLを使った処理が終わると、接続は不要なので切断する
@@ -50,7 +48,7 @@
     $alert = "<script type='text/javascript'>alert('" . $direction . "');</script>";
     echo $alert;*/
 
-/*
+  /*
 echo <<<EOM
     <script>
     var sampleArea = document.getElementById("sampleArea");
@@ -64,4 +62,38 @@ echo <<<EOM
   EOM;
 /** JavaScript出力終了 */
   /*}*/
-?>
+  ?>
+
+  <html>
+
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>テスト出力画面</title>
+  </head>
+
+  <body style="margin: 0;">
+    <!-- アイコン表示 -->
+    <form action="test_output.php" method="POST">
+      <input type="submit" value="Icon表示" name="show">
+    </form>
+    <br /><br />
+
+    <div id="sampleArea">サンプル1</div>
+    <br /><br />
+
+    <!-- マップ -->
+    <div id="map2" style="width:1000px; height:600px"></div>
+
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAzB70xpfmp81NvE6UlsmiTxXJIFSIxiJs&callback=initMap">
+    </script>
+    <script type="text/javascript" src="code.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+  </body>
+
+  </html>
